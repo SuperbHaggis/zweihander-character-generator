5552910d9aa746206b14e14ea0695968abf459d0
Turner Jacobs <tsjacobs3@gmail.com>
2020-01-16 15:47:11 +0400

    Created dom.js

---
+++ dom.js
@@ -0,0 +1,23 @@
+//DOM Elements
+export const nonhumanCheck = document.getElementById('nonhuman-check');
+export const separateAlignmentCheck = document.getElementById('separate-alignment-check');
+export const drawbackCheck = document.getElementById('drawback-check');
+export const generateButton = document.getElementById('generate');
+
+
+export const attributeCheck = document.getElementsByTagName('label');
+export const attributeSwap = document.getElementById('attribute-swap');
+export const attributeReplace = document.getElementById('attribute-replace');
+export const attributesDiv = document.getElementById('attributes');
+export const natSelect = document.createElement("BUTTON");
+export const natSelectText = document.createTextNode("Replace with 55%");
+export const chks = attributesDiv.getElementsByTagName('INPUT');
+
+export const history0 = document.querySelector('#history0');
+export const history1 = document.querySelector('#history1');
+export const history2 = document.querySelector('#history2');
+export const history3 = document.querySelector('#history3');
+export const history4 = document.querySelector('#history4');
+export const history5 = document.querySelector('#history5');
+export const cashP = document.querySelector('#cash-p');
+export const trappingsP = document.querySelector('#trappings');
